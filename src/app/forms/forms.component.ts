86a9541e65765411d38ccd6c77b07d869b68d0d0
Sandeep Kolekar <Sandeep.Kolekar@my1softenger.com>
2018-04-17 18:38:32 +0530

    added form component close btn

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-forms',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+  closePage(){
+    this.router.navigate(['home/dashboard']);
   }
 
 }
